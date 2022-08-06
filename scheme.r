014a3ea774a46caef6d2f7b116153ac6471b7afa
IrenePintarelli <80513536+IrenePintarelli@users.noreply.github.com>
2022-08-06 20:42:00 +0200

    Update scheme.r

--- scheme.r
+++ scheme.r
@@ -1,4 +1,5 @@
 # analysis lake mead change in time
 # 2000 vs 2022
-# load the orginal image and compare them
+# load the orginal images and compare them
 # calculate the amount of lake surface and the variation from the past
+# first usuperClass, freq, calculate the percentage
